5fc6a241f51b16f7122a93f2bda7384e7fd6b2c9
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-19 23:13:50 -0700

    fetch data

--- public/index.js
+++ public/index.js
@@ -4,6 +4,13 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=605f1e31b8284461a6fab7dff2d8c787")
+    let result = await response.json()
+    
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
 }
 
 main()

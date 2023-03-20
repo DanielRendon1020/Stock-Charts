91a43c8c614e0843fb64cd4b2a4678f25d1684dc
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-20 02:10:35 -0700

    highest

--- public/index.js
+++ public/index.js
@@ -11,6 +11,60 @@ async function main() {
 
     const stocks = [GME, MSFT, DIS, BNTX];
 
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }    
+
+    stocks.forEach(stock => stock.values.reverse())
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map( value => value.datetime),
+            datasets: stocks.map(stock =>({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+
+        },
+    })                                                 
+
+    function getHighest(values){
+        let highest = 0
+        values.forEach(value => {
+            if (parseFloat(value.high) > highest){
+                highest = value.high
+            }
+        })
+        return highest
+    }
+
+    new Chart(highestPriceChartCanvas.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: stocks.map( stock => stock.meta.symbol),
+            datasets: [{
+                label: 'Highest',
+                data: stocks.map(stock => getHighest(stock.values)),
+                backgroundColor: stocks.map(stock => getColor(stock.meta.symbol)),
+                borderColor: stocks.map(stock => getColor(stock.meta.symbol)),
+            }]
+
+        },
+    }) 
 }
 
 main()
